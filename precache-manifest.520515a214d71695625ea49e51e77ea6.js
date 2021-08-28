72024b33ba5a20b0839a8ca11534bf6ca8064763
Shubham Agarwal <shubhamagarwal.alld@gmail.com>
2021-08-28 19:42:06 +0530

    Updates

---
+++ precache-manifest.520515a214d71695625ea49e51e77ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18d1d3fcfd01fe242ddcbeb8d1a40a5c",
+    "url": "/grocery-bud/index.html"
+  },
+  {
+    "revision": "14cdf8c67ddd847a0dce",
+    "url": "/grocery-bud/static/css/main.e6bb01d6.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/grocery-bud/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14cdf8c67ddd847a0dce",
+    "url": "/grocery-bud/static/js/main.9d872093.chunk.js"
+  },
+  {
+    "revision": "f19ce6e521be50b493db",
+    "url": "/grocery-bud/static/js/runtime-main.8b040e9c.js"
+  }
+]);
